2bdb94ab8c20aac1526b960130886af974f63893
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2020-12-20 09:46:18 -0700

    tbody

---
+++ static/app.js
@@ -0,0 +1,12 @@
+//appends a table to your web page
+var tableData = data;
+console.log(tableData);
+
+// Reference to the table body
+var tbody = d3.select('tbody')
+
+
+
+
+
+//adds new rows of data for each UFO sighting
