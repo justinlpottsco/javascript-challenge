0b53a0c59336f06e1cd1489a22ddc9779d2e053c
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2020-12-20 10:37:35 -0700

    loop

--- static/app.js
+++ static/app.js
@@ -1,12 +1,15 @@
 //appends a table to your web page
+//adds new rows of data for each UFO sighting
 var tableData = data;
 console.log(tableData);
 
 // Reference to the table body
 var tbody = d3.select('tbody')
 
+// Loop through array of objects then each object
+tableData.forEach((sighting) => {
+    // Append Table Row `tr` to the Table Body `tbody`
+    let row = tbody.append(sighting);
+}
 
 
-
-
-//adds new rows of data for each UFO sighting
